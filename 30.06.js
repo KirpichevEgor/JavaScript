0183770b9dff9e9cc7eddfe6f5bd917702ebab84
KirpichevEgor <TomJerryUSA19@gmail.com>
2022-06-30 18:55:51 +0300

    HW#1 without *

--- 30.06.js
+++ 30.06.js
@@ -23,9 +23,23 @@ let item_7_type
 item_7_type = typeof item_7
 // Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——
 console.log("item_7 == ",item_7 + ", " + "item_7_type == ",item_7_type)
-let age = 10
+let age_1 = 10
 let age_2 = 18
 let age_3 = 60
 // 29. Создать if в котором будите проверять значение переменной age_1
 // 30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
+if (age_1 < age_2 ){
+ console.log ("You don't have access cause your age is " + age_1 + "." + "It's less then " + age_2 + ".")}
+ else if (age_1 >=  age_2 & age_1 <  age_3){
+    console.log("Welcom !")
+ }
+ else if (age_1  > age_3){
+    console.log("Keep calm and look Culture channel")
+ }
+ else {
+    console.log("Technical work")
+ }
+
+
+
 
